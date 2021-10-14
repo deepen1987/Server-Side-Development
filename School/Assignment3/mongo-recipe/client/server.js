d2910c758c53dc8a3a2c43049e29a227efcaa0b3
Deependrasingh Shekhawat <deepen1987@gmail.com>
2021-10-14 09:21:58 -0400

    commit

--- School/Assignment3/mongo-recipe/client/server.js
+++ School/Assignment3/mongo-recipe/client/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 const app = express()
 
-
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(express.static("public"));
 
 const port = process.env.PORT || 3001
